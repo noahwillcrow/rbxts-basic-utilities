aa810e39b20bd824d6f96dc6b096b4149bb86d99
Noah Crowley <noahwillcrow@gmail.com>
2020-06-06 01:23:02 -0400

    Make Lazy compatible with types that extend undefined

--- src/classes/Lazy.ts
+++ src/classes/Lazy.ts
@@ -1,23 +1,30 @@
 /**
  * Lazy-loads a value and stores that value for the rest of the instance's lifetime
  */
-export class Lazy<T extends defined> {
+export class Lazy<T> {
+	private hasValueBeenLoaded: boolean;
 	private value?: T;
 
 	/**
 	 * Creates a new lazy-loading instance of the given type
 	 * @param loadCallback A callback function that returns type T which is used to populate the value of the instance
 	 */
-	public constructor(private readonly loadCallback: () => T) {}
+	public constructor(private readonly loadCallback: () => T) {
+		this.hasValueBeenLoaded = false;
+	}
 
 	/**
-	 * Gets the value of the instance - will lazy-load if value is currently undefined
+	 * Gets the value of the instance - will lazy-load if value has not yet been loaded
 	 */
 	public getValue(): T {
-		if (this.value === undefined) {
+		if (!this.hasValueBeenLoaded) {
 			this.value = this.loadCallback();
+			this.hasValueBeenLoaded = true;
 		}
 
-		return this.value;
+		// Needs the `as T` to make TypeScript ignore that
+		// `this.value`'s type is `T | undefined` at this point
+		// `hasValueBeenLoaded` will control that
+		return this.value as T;
 	}
 }
